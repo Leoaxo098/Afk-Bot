66bfb4616ebf19a715b37dfa378bb5ccc8cc6ad7
Leoaxo098 <98978560+Leoaxo098@users.noreply.github.com>
2025-04-21 20:55:52 +0700

    Update bot.js
    Add webhook edit in setting

--- bot.js
+++ bot.js
@@ -1,8 +1,10 @@
 const mineflayer = require('mineflayer');
+const { Webhook } = require('discord-webhook-node');
 const Movements = require('mineflayer-pathfinder').Movements;
 const pathfinder = require('mineflayer-pathfinder').pathfinder;
 const { GoalBlock, GoalXZ } = require('mineflayer-pathfinder').goals;
 const config = require('./settings.json');
+const webhook = new Webhook(config.webhook);
 const loggers = require('./logging.js');
 const logger = loggers.logger;
 const express = require('express');
@@ -10,163 +12,184 @@ const app = express();
 const port = process.env.PORT || 3000;
 
 app.get('/', (req, res) => {
-  res.status(200).send('Bot is running');
+    res.status(200).send('Bot is running');
 });
 
 app.listen(port, () => {
-  console.log(`Server is running on port ${port}`);
+    console.log(`Server is running on port ${port}`);
 });
 
 function createBot() {
-   const bot = mineflayer.createBot({
-      username: config['bot-account']['username'],
-      password: config['bot-account']['password'],
-      auth: config['bot-account']['type'],
-      host: config.server.ip,
-      port: config.server.port,
-      version: config.server.version,
-   });
+    const bot = mineflayer.createBot({
+        username: config['bot-account']['username'],
+        password: config['bot-account']['password'],
+        auth: config['bot-account']['type'],
+        host: config.server.ip,
+        port: config.server.port,
+        version: config.server.version,
+    });
 
-   bot.loadPlugin(pathfinder);
-   const mcData = require('minecraft-data')(bot.version);
-   const defaultMove = new Movements(bot, mcData);
-   bot.settings.colorsEnabled = false;
-   bot.pathfinder.setMovements(defaultMove);
+    bot.loadPlugin(pathfinder);
+    const mcData = require('minecraft-data')(bot.version);
+    const defaultMove = new Movements(bot, mcData);
+    bot.settings.colorsEnabled = false;
+    bot.pathfinder.setMovements(defaultMove);
 
-   bot.once('spawn', () => {
-      logger.info("Bot joined to the server");
+    bot.once('spawn', () => {
+        logger.info("Bot joined to the server");
+        webhook.send({
+            content: 'Bot joined to the server',
+            username: 'Bot Status',
+        });
 
-      if (config.utils['auto-auth'].enabled) {
-         logger.info('Started auto-auth module');
+        if (config.utils['auto-auth'].enabled) {
+            logger.info('Started auto-auth module');
 
-         let password = config.utils['auto-auth'].password;
-         setTimeout(() => {
-            bot.chat(`/register ${password} ${password}`);
-            bot.chat(`/login ${password}`);
-         }, 500);
+            let password = config.utils['auto-auth'].password;
+            setTimeout(() => {
+                bot.chat(`/register ${password} ${password}`);
+                bot.chat(`/login ${password}`);
+            }, 500);
 
-         logger.info(`Authentication commands executed`);
-      }
+            logger.info(`Authentication commands executed`);
+        }
 
-      if (config.utils['chat-messages'].enabled) {
-         logger.info('Started chat-messages module');
+        if (config.utils['chat-messages'].enabled) {
+            logger.info('Started chat-messages module');
 
-         let messages = config.utils['chat-messages']['messages'];
+            let messages = config.utils['chat-messages']['messages'];
 
-         if (config.utils['chat-messages'].repeat) {
-            let delay = config.utils['chat-messages']['repeat-delay'];
-            let i = 0;
+            if (config.utils['chat-messages'].repeat) {
+                let delay = config.utils['chat-messages']['repeat-delay'];
+                let i = 0;
 
-            setInterval(() => {
-               bot.chat(`${messages[i]}`);
+                setInterval(() => {
+                    bot.chat(`${messages[i]}`);
 
-               if (i + 1 === messages.length) {
-                  i = 0;
-               } else i++;
-            }, delay * 1000);
-         } else {
-            messages.forEach((msg) => {
-               bot.chat(msg);
+                    if (i + 1 === messages.length) {
+                        i = 0;
+                    } else i++;
+                }, delay * 1000);
+            } else {
+                messages.forEach((msg) => {
+                    bot.chat(msg);
+                });
+            }
+        }
+
+        const pos = config.position;
+
+        if (config.position.enabled) {
+            logger.info(
+                `Starting moving to target location (${pos.x}, ${pos.y}, ${pos.z})`
+            );
+            bot.pathfinder.setGoal(new GoalBlock(pos.x, pos.y, pos.z));
+        }
+
+        if (config.utils['anti-afk'].enabled) {
+            if (config.utils['anti-afk'].sneak) {
+                bot.setControlState('sneak', true);
+            }
+
+            if (config.utils['anti-afk'].jump) {
+                bot.setControlState('jump', true);
+            }
+
+            if (config.utils['anti-afk']['hit'].enabled) {
+                let delay = config.utils['anti-afk']['hit']['delay'];
+                let attackMobs = config.utils['anti-afk']['hit']['attack-mobs']
+
+                setInterval(() => {
+                    if (attackMobs) {
+                        let entity = bot.nearestEntity(e => e.type !== 'object' && e.type !== 'player'
+                            && e.type !== 'global' && e.type !== 'orb' && e.type !== 'other');
+
+                        if (entity) {
+                            bot.attack(entity);
+                            return
+                        }
+                    }
+
+                    bot.swingArm("right", true);
+                }, delay);
+            }
+
+            if (config.utils['anti-afk'].rotate) {
+                setInterval(() => {
+                    bot.look(bot.entity.yaw + 1, bot.entity.pitch, true);
+                }, 100);
+            }
+
+            if (config.utils['anti-afk']['circle-walk'].enabled) {
+                let radius = config.utils['anti-afk']['circle-walk']['radius']
+                circleWalk(bot, radius);
+            }
+        }
+    });
+
+    bot.on('chat', (username, message) => {
+        if (config.utils['chat-log']) {
+            logger.info(`<${username}> ${message}`);
+        }
+    });
+
+    bot.on('goal_reached', () => {
+        if (config.position.enabled) {
+            logger.info(
+                `Bot arrived to target location. ${bot.entity.position}`
+            );
+        }
+    });
+
+    bot.on('death', () => {
+        logger.warn(
+            `Bot has been died and was respawned at ${bot.entity.position}`
+        );
+        webhook.send({
+            content: `Bot has been died and was respawned at ${bot.entity.position}`,
+            username: 'Bot Status',
+        });
+    });
+
+    if (config.utils['auto-reconnect']) {
+        bot.on('end', () => {
+            logger.info("Bot disconnected, reconnecting...")
+            webhook.send({
+                content: "Bot disconnected, reconnecting...",
+                username: 'Bot Status',
             });
-         }
-      }
+            setTimeout(() => {
+                createBot();
+            }, config.utils['auto-reconnect-delay']);
+        });
+    }
 
-      const pos = config.position;
+    bot.on('kicked', (reason) => {
+        let reasonText = JSON.parse(reason).text;
+        if (reasonText === '') {
+            reasonText = JSON.parse(reason).extra[0].text
+        }
+        reasonText = reasonText.replace(/§./g, '');
 
-      if (config.position.enabled) {
-         logger.info(
-             `Starting moving to target location (${pos.x}, ${pos.y}, ${pos.z})`
-         );
-         bot.pathfinder.setGoal(new GoalBlock(pos.x, pos.y, pos.z));
-      }
+        logger.warn(`Bot was kicked from the server. Reason: ${reasonText}`)
+        webhook.send({
+            content: `Bot was kicked from the server. Reason: ${reasonText}`,
+            username: 'Bot Status',
+        });
+    }
+    );
 
-      if (config.utils['anti-afk'].enabled) {
-         if (config.utils['anti-afk'].sneak) {
-            bot.setControlState('sneak', true);
-         }
-
-         if (config.utils['anti-afk'].jump) {
-            bot.setControlState('jump', true);
-         }
-
-         if (config.utils['anti-afk']['hit'].enabled) {
-            let delay = config.utils['anti-afk']['hit']['delay'];
-            let attackMobs = config.utils['anti-afk']['hit']['attack-mobs']
-
-            setInterval(() => {
-               if(attackMobs) {
-                     let entity = bot.nearestEntity(e => e.type !== 'object' && e.type !== 'player'
-                         && e.type !== 'global' && e.type !== 'orb' && e.type !== 'other');
-
-                     if(entity) {
-                        bot.attack(entity);
-                        return
-                     }
-               }
-
-               bot.swingArm("right", true);
-            }, delay);
-         }
-
-         if (config.utils['anti-afk'].rotate) {
-            setInterval(() => {
-               bot.look(bot.entity.yaw + 1, bot.entity.pitch, true);
-            }, 100);
-         }
-
-         if (config.utils['anti-afk']['circle-walk'].enabled) {
-            let radius = config.utils['anti-afk']['circle-walk']['radius']
-            circleWalk(bot, radius);
-         }
-      }
-   });
-
-   bot.on('chat', (username, message) => {
-      if (config.utils['chat-log']) {
-         logger.info(`<${username}> ${message}`);
-      }
-   });
-
-   bot.on('goal_reached', () => {
-      if(config.position.enabled) {
-         logger.info(
-             `Bot arrived to target location. ${bot.entity.position}`
-         );
-      }
-   });
-
-   bot.on('death', () => {
-      logger.warn(
-         `Bot has been died and was respawned at ${bot.entity.position}`
-      );
-   });
-
-   if (config.utils['auto-reconnect']) {
-      bot.on('end', () => {
-         setTimeout(() => {
-            createBot();
-         }, config.utils['auto-reconnect-delay']);
-      });
-   }
-
-   bot.on('kicked', (reason) => {
-      let reasonText = JSON.parse(reason).text;
-      if(reasonText === '') {
-         reasonText = JSON.parse(reason).extra[0].text
-      }
-      reasonText = reasonText.replace(/§./g, '');
-
-      logger.warn(`Bot was kicked from the server. Reason: ${reasonText}`)
-   }
-   );
-
-   bot.on('error', (err) =>
-      logger.error(`${err.message}`)
-   );
+    bot.on('error', (err) => {
+        logger.error(`${err.message}`);
+        webhook.send({
+            content: `Bot has an error: ${err.message}`,
+            username: 'Bot Status',
+        });
+    });
 }
 
 function circleWalk(bot, radius) {
-   // Make bot walk in square with center in bot's  wthout stopping
+    // Make bot walk in square with center in bot's  wthout stopping
     return new Promise(() => {
         const pos = bot.entity.position;
         const x = pos.x;
@@ -182,9 +205,9 @@ function circleWalk(bot, radius) {
 
         let i = 0;
         setInterval(() => {
-             if(i === points.length) i = 0;
-             bot.pathfinder.setGoal(new GoalXZ(points[i][0], points[i][2]));
-             i++;
+            if (i === points.length) i = 0;
+            bot.pathfinder.setGoal(new GoalXZ(points[i][0], points[i][2]));
+            i++;
         }, 1000);
     });
 }
